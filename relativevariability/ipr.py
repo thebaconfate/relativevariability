c4f960a536d849ec1bc73015e6979cbaba01a071
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-05-04 02:31:29 +0200

    added docstring

--- relativevariability/ipr.py
+++ relativevariability/ipr.py
@@ -5,6 +5,7 @@ import math
 def ipr(
     data_list: list[Union[int, float]], percentile_1: float, percentile_2: float
 ) -> float:
+    """Calculate the Interpercentile Range (IPR) of a list of data. Returns value at percentile_2 minus value at percentile_1. Swaps percentiles if percentile_1 > percentile_2. Raises ValueError if percentiles are not between 0 and 1."""
     if percentile_1 < 0 or percentile_1 > 1 or percentile_2 < 0 or percentile_2 > 1:
         raise ValueError("Percentile values must be between 0 and 1.")
     if percentile_1 > percentile_2:
@@ -14,7 +15,5 @@ def ipr(
     data_len: int = len(data_list)
     percentile_1_idx: int = max(0, math.ceil(percentile_1 * data_len) - 1)
     percentile_2_idx: int = max(0, math.ceil(percentile_2 * data_len) - 1)
-    print(f"percentile_1_idx: {data_list[percentile_1_idx]}")
-    print(f"percentile_2_idx: {data_list[percentile_2_idx]}")
     sorted_data: list[Union[int, float]] = sorted(data_list)
     return sorted_data[percentile_2_idx] - sorted_data[percentile_1_idx]
