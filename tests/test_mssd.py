d04547577dab7364d70fe1cfab9b80f1b47e4155
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-05-05 14:35:08 +0200

    implemented mssd

--- tests/test_mssd.py
+++ tests/test_mssd.py
@@ -1,7 +1,14 @@
-
+import pytest
 
 from relativevariability import mssd
 
 
 def test_mssd():
     assert mssd([1, 2, 3, 4, 5]) == 1
+    assert mssd([110, 10, 20, 50, 200, 100, 900, 500]) == 120500
+
+
+def test_mssd_error():
+ with pytest.raises(ValueError):
+     mssd([1])
+     mssd([])
