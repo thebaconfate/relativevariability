edad839e7fdbb9c4394262dd5812fdecedd0d620
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-06-24 22:32:21 +0200

    implemented maximum range

--- relativevariability/maximumrange.py
+++ relativevariability/maximumrange.py
@@ -1,2 +1,5 @@
-def maximum_range():
-    pass
+from relativevariability.maximumipr import maximum_ipr
+
+
+def maximum_range(data_mean, data_min, data_max, data_len):
+    return maximum_ipr(data_mean, data_min, data_max, data_len, 1, data_len)
