01413cb277c1324fcdd79ac0a328c545ef6e63cf
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-06-25 15:54:45 +0200

    added tests for maximum_var

--- relativevariability/updateusefill.py
+++ relativevariability/updateusefill.py
@@ -1,6 +1,8 @@
 def update_use_fill(to_add_max_min, to_use, to_fill, i):
-    # Adjust elements in toUse and toFill based on toAddMaxMin
+    pass
+    """# Adjust elements in toUse and toFill based on toAddMaxMin
     for j in range(len(to_add_max_min)):
         to_use[to_add_max_min[j]] -= 1
         to_fill[i] -= 1
     return {"to_use": to_use, "to_fill": to_fill}
+"""
--- tests/test_maximumvar.py
+++ tests/test_maximumvar.py
@@ -9,8 +9,19 @@ def test_maxvar():
 def test_maxvar_1():
     assert maximum_var([0, 1]) == pytest.approx(0.5)
 
+
 def test_maxvar_negs():
     assert maximum_var([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]) == pytest.approx(22.5)
 
+
 def test_maxvar_negs_unordered():
     assert maximum_var([-1, 6, 8, -6, -10, 9, 2, 6, 2]) == pytest.approx(84.69444)
+
+
+def test_mean_equals_min():
+    assert maximum_var([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == 0
+
+
+def test_n_max_equals_zero():
+    """ "Can't seem to generate a list that would make this test pass the right branches."""
+    pass
