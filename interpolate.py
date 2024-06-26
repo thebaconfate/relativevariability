6e8ac388ab4ec8f0be4f8efa1900580c7ccb12ee
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-06-26 07:17:24 +0200

    added try for test 2

--- interpolate.py
+++ interpolate.py
@@ -22,7 +22,29 @@ def interpolate(x):
     return lhs - rhs
 
 
-constraints = [{"type": "ineq", "fun": lambda x: interpolate(x)}]
+def interpolate2(x):
+    _length = len(x)
+    _min = min(x)
+    _max = max(x)
+    _p1 = math.floor(0.25 * _length)
+    _p2 = math.floor(0.75 * _length)
+    _mean = statistics.mean(x)
+    if _mean == _min or _mean == _max:
+        raise ValueError("Mean is equal to min or max")
+    if abs(_min) > abs(_max):
+        temp = -_min
+        _min = -_max
+        _max = temp
+        _mean = -_mean
+    lhs = _length * _mean
+    rhs = _p1 * _min + (_length - _p1) * _max
+    return lhs - (rhs + 1e-6)
+
+
+constraints = [
+    {"type": "ineq", "fun": lambda x: interpolate(x)},
+    {"type": "eq", "fun": lambda x: interpolate2(x)},
+]
 
 x0 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = minimize(
@@ -30,4 +52,5 @@ result = minimize(
 )
 
 print(result.x)
-print(interpolate(result.x))
+print("interpolate", interpolate(result.x))
+print("interpolate2", interpolate2(result.x))
