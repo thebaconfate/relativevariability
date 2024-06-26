13a87c9c1038bb121f3a59935bfac4179026051c
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-06-26 04:37:46 +0200

    third try

--- interpolate.py
+++ interpolate.py
@@ -10,7 +10,7 @@ def interpolate(lst):
     _max = max(lst)
 
     if _mean == _min or _mean == _max:
-        return 1000
+        raise Exception("mean == min or mean == max")
     if abs(_min) > abs(_max):
         temp = -_min
         _min = -_max
@@ -18,7 +18,7 @@ def interpolate(lst):
         _mean = -_mean
     lhs = _length * _mean
     rhs = (_p2 - 1) * _min + (_length - _p2 + 1) * _max
-    return lhs - rhs
+    return lhs - rhs + 0.0001e-100
 
 
 constraints = [{"type": "eq", "fun": interpolate}]
@@ -26,7 +26,7 @@ constraints = [{"type": "eq", "fun": interpolate}]
 res = minimize(
     sum,
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    options={"disp": True},
+    options={"disp": True, "maxiter": 10000},
     constraints=constraints,
     method="SLSQP",
 )
